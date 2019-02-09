e9dd64f38b55105b38eed456d01462a278de1933
yangyiquan <251742737@qq.com>
2019-02-09 09:51:20 +0800

    src

---
+++ src/RabbitConsumer/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer start");
+
+            IConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = "120.79.179.19",
+                Port = 5672,
+                UserName = "yyq1",
+                Password = "yuntong810"
+            };
+
+            using (IConnection conn = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    string queueName = string.Empty;
+
+                    if (args.Length > 0)
+                        queueName = args[0];
+                    else
+                        queueName = "queue1";
+
+                    //声明一个队列
+                    channel.QueueDeclare(
+                      queue: queueName,//消息队列名称
+                      durable: false,//是否缓存
+                      exclusive: false,
+                      autoDelete: false,
+                      arguments: null
+                       );
+
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, em) =>
+                    {
+                        byte[] message = em.Body;
+                        Console.WriteLine("received message:" + Encoding.UTF8.GetString(message));
+
+
+                    };
+
+                   channel.BasicConsume(
+                       queue: queueName,
+                       autoAck: true,
+                       consumer: consumer
+                       );
+
+                    Console.ReadKey();
+
+                }
+            }
+
+        }
+    }
+}
---
+++ src/RabbitMQTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RabbitMQ start");
+
+
+
+
+            IConnectionFactory connFactory = new ConnectionFactory
+            {
+                HostName = "120.79.179.19",
+                Port = 5672,
+                UserName = "yyq",
+                Password = "yuntong810"
+            };
+
+            using (IConnection conn = connFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    Console.WriteLine("create channel success");
+
+                    string queueName = string.Empty;
+
+                    if (args.Length > 0)
+                    {
+                        queueName = args[0];
+                    }
+                    else
+                        queueName = "queue1";
+
+                    channel.QueueDeclare(
+                        queue: queueName,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    while (true)
+                    {
+                        Console.WriteLine("message contant");
+
+                        string message = Console.ReadLine();
+
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(
+                            exchange:"",
+                            routingKey :queueName ,
+                            basicProperties : null,
+                            body :body
+                            );
+
+                        Console.WriteLine("send message success:" + message);
+                    }
+                }
+            }
+
+        }
+    }
+}
